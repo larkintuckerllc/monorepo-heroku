d48a1042154d982ec3e2458e8f43e0465450c9a5
John Tucker <john.tucker@sharpspring.com>
2018-09-10 05:41:57 -0400

    Selective build

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,27 @@
-npm run bootstrap &&
-  npm run build-ts
+npm run bootstrap
+exit_status=$?
+if [ $exit_status -eq 0 ]; then
+  if [ -f build ]; then
+    echo "POSTINSTALL: Building packages in build file"
+    while read -r line; do packages+=( "$line" ); done < build
+    for package in "${packages[@]}"; do
+      if [ -d "packages/$package" ]; then
+        echo "POSTINSTALL: Building $packages"
+        npm run lerna -- run --scope $package build-ts
+        exit_status=$?
+        if [ $exit_status -ne 0 ]; then
+          exit $exit_status
+        fi
+      else
+        echo "POSTINSTALL: ERROR - Package $package in build file does not exist"
+        exit 1
+      fi
+    done
+  else
+    echo "POSTINSTALL: Building all packages"
+    npm run build-ts
+    exit $?
+  fi
+else
+  exit $exit_status
+fi
